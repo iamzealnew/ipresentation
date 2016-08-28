22a2684bea2a7bb314b41836dd2dcd60540f922b
iamzeal <shikharshrivastava93@gmail.com>
2016-08-28 22:08:49 +0530

    Data Source in the code behind file
    Basic operation using grid view, binding data using code behind file!

---
+++ GridViewBindingDemo.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GridViewBindingDemo.aspx.cs" Inherits="GridviewManipulationsDemo.GridViewBindingDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #FF6600;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="gvEmpDetails" runat="server" AutoGenerateColumns="False" OnRowCancelingEdit="gvEmpDetails_RowCancelingEdit" OnRowEditing="gvEmpDetails_RowEditing" OnSelectedIndexChanged="gvEmpDetails_SelectedIndexChanged" OnRowDataBound="gvEmpDetails_RowDataBound" OnRowDeleting="gvEmpDetails_RowDeleting" OnRowUpdating="gvEmpDetails_RowUpdating">
+            <Columns>
+                <asp:TemplateField>
+                    <HeaderTemplate>
+                        <asp:CheckBox ID="CheckBox2" runat="server" AutoPostBack="True" OnCheckedChanged="CheckBox2_CheckedChanged" />
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="CheckBox1" runat="server" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="EmpNo">
+                    <EditItemTemplate>
+                        <asp:Label ID="lbl_ER_EmpNo" runat="server" Text='<%# Eval("EmpNo") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lbl_IR_EmpNo" runat="server" Text='<%# Eval("EmpNo") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Name">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txt_ER_Name" runat="server" Text='<%# Eval("Name") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lbl_IR_Name" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Salary">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txt_ER_Salary" runat="server" Text='<%# Eval("Salary") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lbl_IR_Salary" runat="server" Text='<%# Eval("Salary") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Designation">
+                    <EditItemTemplate>
+                        <asp:DropDownList ID="ddl_ER_Job" runat="server">
+                        </asp:DropDownList>
+                        <asp:Label ID="lbl_ER_Job" runat="server" Text='<%# Eval("Designation") %>' Visible="False"></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lbl_IR_Job" runat="server" Text='<%# Eval("Designation") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField ShowHeader="False">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkSelect" runat="server" CausesValidation="False" CommandName="Select" Text="Show"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField ShowHeader="False">
+                    <EditItemTemplate>
+                        <asp:LinkButton ID="lnkModify" runat="server" CausesValidation="True" CommandName="Update" Text="Modify"></asp:LinkButton>
+                        &nbsp;<asp:LinkButton ID="lnkCancel" runat="server" CausesValidation="False" CommandName="Cancel" Text="Cancel"></asp:LinkButton>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkEdit" runat="server" CausesValidation="False" CommandName="Edit" Text="Edit"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField ShowHeader="False">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkRemove" runat="server" CausesValidation="False" CommandName="Delete" Text="Delete" OnClientClick="return confirm(&quot;Are you sure?&quot;);"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+        <br />
+        <br />
+        <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" OnClientClick="return confirm(&quot;Do you want to delete all the selected records?&quot;);" Text="Delete" />
+&nbsp;<span class="auto-style1"><strong>Note: This delete button is to delete the selected records</strong></span></form>
+</body>
+</html>
---
+++ GridViewBindingDemo.aspx.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GridviewManipulationsDemo
+{
+    public partial class GridViewBindingDemo : System.Web.UI.Page
+    {
+
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet jobsDS, empDS;
+
+        /// <summary>
+        /// Creates a Connection with the Database
+        /// </summary>
+        /// <returns></returns>
+        static SqlConnection GetConnection()
+        {
+            string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["DB08H92Constr"].ConnectionString;
+            try
+            {
+                if (!string.IsNullOrEmpty(conStr))
+                {
+                    return new SqlConnection(conStr);
+                }
+                else
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        void BindEmpData()
+        {
+            cn = GetConnection();
+            da = new SqlDataAdapter("select * from tblEmployeeDetails", cn);
+            empDS = new DataSet();
+            da.Fill(empDS);
+            gvEmpDetails.DataSource = empDS.Tables[0];
+            gvEmpDetails.DataBind();
+        }
+
+
+        DataTable LoadJobs()
+        {
+            cn = GetConnection();
+            cmd = new SqlCommand("usp_GetJobs", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(cmd);
+            jobsDS = new DataSet();
+            da.Fill(jobsDS, "Jobs");
+            return jobsDS.Tables[0];
+        }
+
+
+        /// <summary>
+        /// Modifies the employee details
+        /// </summary>
+        /// <param name="empNo"></param>
+        /// <param name="name"></param>
+        /// <param name="sal"></param>
+        /// <param name="job"></param>
+        /// <returns></returns>
+        bool ModifyEmployee(int empNo, string name, double sal, string job)
+        {
+            cn = GetConnection();
+            if (cn.State == System.Data.ConnectionState.Closed)
+            {
+                cn.Open();
+            }
+
+            cmd = new SqlCommand("usp_ModifyEmployee", cn); cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@id", empNo));
+            cmd.Parameters.Add(new SqlParameter("@name", name));
+            cmd.Parameters.Add(new SqlParameter("@sal", sal));
+            cmd.Parameters.Add(new SqlParameter("@job", job));
+            SqlParameter returnParam = cmd.Parameters.Add("ReturnValue", SqlDbType.Int);
+            returnParam.Direction = ParameterDirection.ReturnValue;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            int result = (int)returnParam.Value;
+            if (result > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Removes employee details
+        /// </summary>
+        /// <param name="empNo"></param>
+        /// <returns></returns>
+        bool RemoveEmployee(int empNo)
+        {
+            cn = GetConnection();
+            if (cn.State == System.Data.ConnectionState.Closed)
+            {
+                cn.Open();
+            }
+            cmd = new SqlCommand("usp_RemoveEmployee", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@id", empNo));
+            SqlParameter returnParam = cmd.Parameters.Add("ReturnValue", SqlDbType.Int);
+            returnParam.Direction = ParameterDirection.ReturnValue;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            int result = (int)returnParam.Value;
+            if (result > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+                BindEmpData();
+        }
+
+        protected void gvEmpDetails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow gvr = gvEmpDetails.SelectedRow;
+            Label lblName = (Label)gvr.FindControl("lbl_IR_Name");
+            ClientScript.RegisterStartupScript(this.GetType(), "ShowMsg", "alert('Selected Employee is:" + lblName.Text + "')", true);
+        }
+
+        protected void gvEmpDetails_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvEmpDetails.EditIndex = e.NewEditIndex;
+            BindEmpData();
+        }
+
+        protected void gvEmpDetails_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvEmpDetails.EditIndex = -1;
+            BindEmpData();
+        }
+
+        protected void gvEmpDetails_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow &&  gvEmpDetails.EditIndex == e.Row.RowIndex)
+            {
+                DropDownList ddlJobs = (DropDownList)e.Row.FindControl("ddl_ER_Job");
+                ddlJobs.DataSource = LoadJobs();
+                ddlJobs.DataTextField = "Designation";
+                ddlJobs.DataValueField = "Designation";
+                ddlJobs.DataBind();
+                ddlJobs.Items.FindByValue((e.Row.FindControl("lbl_ER_Job") as Label).Text).Selected = true;
+            }
+        }
+
+        protected void gvEmpDetails_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow gvr = gvEmpDetails.Rows[e.RowIndex];
+            int empNo;
+            string name, job;
+            double sal;
+            empNo = Int32.Parse(((Label)gvr.FindControl("lbl_ER_Empno")).Text);
+            name = ((TextBox)gvr.FindControl("txt_ER_Name")).Text;
+            sal = Double.Parse(((TextBox)gvr.FindControl("txt_ER_Salary")).Text);
+            job = ((DropDownList)gvr.FindControl("ddl_ER_Job")).SelectedValue;
+            bool result = ModifyEmployee(empNo, name, sal, job);
+            if (result)
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "SuccessMsg", "alert('Details are modified successfully')", true);
+            else
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "ErrMsg", "alert('Please recheck the details and submit them again')", true);
+            gvEmpDetails.EditIndex = -1;
+            BindEmpData();
+        }
+
+        protected void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (GridViewRow gvr in gvEmpDetails.Rows)
+            {
+                CheckBox chk=(CheckBox)gvr.FindControl("CheckBox1");
+                chk.Checked = ((CheckBox)gvEmpDetails.HeaderRow.FindControl("CheckBox2")).Checked;
+            }
+        }
+
+        protected void gvEmpDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow gvr = gvEmpDetails.Rows[e.RowIndex];
+            int eno = Int32.Parse(((Label)gvr.FindControl("lbl_IR_Empno")).Text);
+            bool s = RemoveEmployee(eno);
+            if (s)
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "Message", "alert('Details are removed successfully')", true);
+            else
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "Message", "alert('Please recheck the details and submit them again')", true);
+            BindEmpData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (GridViewRow gvr in gvEmpDetails.Rows)
+            {
+                CheckBox chk = (CheckBox)gvr.FindControl("CheckBox1");
+                if (chk.Checked)
+                {
+                    int eno = Int32.Parse(((Label)gvr.FindControl("lbl_IR_Empno")).Text);
+                    bool s = RemoveEmployee(eno);
+                }
+            }
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "Message", "alert('All selected records are removed successfully')", true);
+            BindEmpData();
+        }
+    }
+}
